2c20aacd9f8884786ee9d810721f19cb29cb3902
Alejandro Falkowski <alejandro.falkowski@gelato.com>
2019-03-15 14:34:05 +0000

    Add feature 'Add a todo'.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,6 +3,7 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +19,37 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given I already have a todo of :arg1
+     */
+    public function iAlreadyHaveATodoOf($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I want to add a todo of :arg1
+     */
+    public function iWantToAddATodoOf($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should succesufully have a todo of :arg1
+     */
+    public function iShouldSuccesufullyHaveATodoOf($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should not have another todo of :arg1
+     */
+    public function iShouldNotHaveAnotherTodoOf($arg1)
+    {
+        throw new PendingException();
     }
 }
