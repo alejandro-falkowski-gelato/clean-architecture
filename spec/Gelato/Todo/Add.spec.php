d214733d78a4c577cdd0e1d23e37ff9bd41ee4c9
Alejandro Falkowski <alejandro.falkowski@gelato.com>
2019-04-02 12:05:08 +0100

    Added specs for Add a todo.

---
+++ spec/Gelato/Todo/Add.spec.php
@@ -0,0 +1,12 @@
+
+<?php
+
+describe("Add a todo", function() {
+    it("Adding a new todo", function() {
+        expect(false)->toBe(true);
+    });
+
+    it("Can't add a duplicate todo", function() {
+        expect(false)->toBe(true);
+    });
+});
---
+++ src/Gelato/Todo/Add.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Gelato\Todo;
+
+class Add {
+
+}
